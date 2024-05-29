dce1347a51f6ed2e6e4bcc7c73e578e8b7bd336f
leeheejin00 <dlgmlwls0000@gmail.com>
2024-05-29 22:59:16 +0900

    GYU_0529_fin

--- press_release/js/press_release.js
+++ press_release/js/press_release.js
@@ -16,60 +16,35 @@ document.addEventListener('DOMContentLoaded', function() {
             modal.style.display = 'none';
         }
     });
+    var tabMenu = document.querySelector('.tab_menu');
+
+    document.getElementById('tab-trigger').addEventListener('click', function() {
+      tabMenu.style.display = 'block';
+    });
+
+    const tabList = document.querySelectorAll('.tab_menu .list li');
+    const contents = document.querySelectorAll('.tab_menu .cont_area .cont');
+    let activeCont = ''; // 현재 활성화 된 컨텐츠 (기본:#tab1 활성화)
+
+
+  for(var i = 0; i < tabList.length; i++){
+    tabList[i].querySelector('.btn').addEventListener('click', function(e){
+      e.preventDefault();
+      for(var j = 0; j < tabList.length; j++){
+        // 나머지 버튼 클래스 제거
+        tabList[j].classList.remove('is_on');
+
+        // 나머지 컨텐츠 display:none 처리
+        contents[j].style.display = 'none';
+      }
+
+      // 버튼 관련 이벤트
+      this.parentNode.classList.add('is_on');
+
+      // 버튼 클릭시 컨텐츠 전환
+      activeCont = this.getAttribute('href');
+      document.querySelector(activeCont).style.display = 'block';
+    });
+}
 });
 
-document.querySelectorAll('.form input, .form textarea').forEach(field => {
-    field.addEventListener('keyup', handleEvent);
-    field.addEventListener('blur', handleEvent);
-    field.addEventListener('focus', handleEvent);
-    
-    function handleEvent(e) {
-      let label = field.previousElementSibling;
-      
-      if (e.type === 'keyup') {
-        if (field.value === '') {
-          label.classList.remove('active', 'highlight');
-        } else {
-          label.classList.add('active', 'highlight');
-        }
-      } else if (e.type === 'blur') {
-        if (field.value === '') {
-          label.classList.remove('active', 'highlight');
-        } else {
-          label.classList.remove('highlight');
-        }
-      } else if (e.type === 'focus') {
-        if (field.value !== '') {
-          label.classList.add('highlight');
-        } else {
-          label.classList.remove('highlight');
-        }
-      }
-    }
-  });
-  
-  document.querySelectorAll('.tab a').forEach(tab => {
-    tab.addEventListener('click', function (e) {
-      e.preventDefault();
-      let target = document.querySelector(tab.getAttribute('href'));
-  
-      tab.parentNode.classList.add('active');
-      let siblings = Array.from(tab.parentNode.parentNode.children);
-      for (let sibling of siblings) {
-        if (sibling !== tab.parentNode) {
-          sibling.classList.remove('active');
-        }
-      }
-  
-      document.querySelectorAll('.tab-content > div').forEach(div => {
-        if (div !== target) {
-          div.style.display = 'none';
-        }
-      });
-      target.style.display = 'block';
-      target.style.opacity = 0;
-      setTimeout(() => {
-        target.style.opacity = 1;
-      }, 200); // A slight delay to trigger CSS transition
-    });
-  });
